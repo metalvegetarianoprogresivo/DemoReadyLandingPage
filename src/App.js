3105fedff786dc4aeebb7732a047fca429487dd0
david-intersys <drodriguez@intersysconsulting.com>
2018-07-31 16:10:49 -0500

    Merged from master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Row, Col, Carousel} from 'react-materialize';
+import DemoCard from './components/card/DemoCard'
 import './App.css';
+
+
 
 class App extends Component {
   render() {
+    var cardData = {
+      image: 'https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall.jpg',
+      title: 'Demo Title',
+      description: 'I am a very simple card. I am good at containing small bits of information',
+      linkTo: '#',
+      linkText: 'Link text'
+    }
     return (
       <span></span>
     );
