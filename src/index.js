87a782b45c24018bac8a8993e9d767c775892bf7
david-intersys <drodriguez@intersysconsulting.com>
2018-08-01 16:54:07 -0500

    Installation of materialize and jquery with index.css updated

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import $ from "jquery";
+window.jQuery = window.$ = $;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
