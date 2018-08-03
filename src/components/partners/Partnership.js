e58a8146b8d32cec4e024279f8a8a444424a57a9
david-intersys <drodriguez@intersysconsulting.com>
2018-08-03 12:45:42 -0500

    Images added to partnership component

--- src/components/partners/Partnership.js
+++ src/components/partners/Partnership.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import {Section} from 'react-materialize';
+import {Section, Row, Col} from 'react-materialize';
 import './Partnership.css'
 
 class Partnership extends Component {
     render(){
         return(
-            <Section >
-                <div className="content-wrap">
-                    <div className="image-partner">image</div>
-                    <div className="image-partner">image</div>
-                    <div className="image-partner">image</div>
-                    <div className="image-partner">image</div>
-                    <div className="image-partner">image</div>
-                </div>
+            <Section>
+                <Row>
+                    <Col s={1} m={2}>
+                    <img className="image-partner"  src='https://www.intersysconsulting.com/wp-content/uploads/2016/07/elastic-logo.jpg'/>
+                    </Col>
+                    <Col s={1} m={2}>
+                    <img className="image-partner"  src='https://www.intersysconsulting.com/wp-content/uploads/2016/08/microsoft-logo-300x64.png'/>
+                    </Col>
+                    <Col s={1} m={2}>
+                    <img className="image-partner"  src='https://www.intersysconsulting.com/wp-content/uploads/2016/07/amazon-logo.jpg'/>
+                    </Col>
+                    <Col s={1} m={2}>
+                    <img className="image-partner"  src='https://www.intersysconsulting.com/wp-content/uploads/2016/07/cloudera-logo.jpg'/>
+                    </Col>
+                    <Col s={1} m={2}>
+                    <img className="image-partner"  src='https://www.intersysconsulting.com/wp-content/uploads/2016/07/horton-logo.jpg'/>
+                    </Col>
+                    <Col s={1} m={2}>
+                    <img className="image-partner"  src='https://www.intersysconsulting.com/wp-content/uploads/2016/07/mapr-logo.jpg'/>
+                    </Col>
+                </Row>
             </Section>
         );
     }
