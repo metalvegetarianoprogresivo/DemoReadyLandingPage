35b8d5926b35fd125a7d0877f5cff508cece2447
david-intersys <drodriguez@intersysconsulting.com>
2018-08-06 14:57:30 -0500

    Cardboard created dynamically

--- src/components/card/CardBoard.js
+++ src/components/card/CardBoard.js
@@ -3,44 +3,44 @@ import {Row, Col, Container} from 'react-materialize';
 import DemoCard from './DemoCard'
 import './CardBoard.css'
 
+const colNumber = 3;
+const m = 12 / colNumber;
+const s = 1;
+
 class CardBoard extends Component {
+    
     render(){
-      var cardData = {
-        image: 'https://s3-us-west-2.amazonaws.com/uw-s3-cdn/wp-content/uploads/sites/6/2017/11/04133712/waterfall.jpg',
-        title: 'Demo Title',
-        description: 'I am a very simple card. I am good at containing small bits of information',
-        linkTo: '#',
-        linkText: 'Link text'
-      }
       return(
         <div className='cardboard'>
           <Container >
-            <Row>
-              <Col s={1} m={4}>
-                  <DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/>
-              </Col>
-              <Col s={1} m={4}>
-                  <DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/>
-              </Col>
-              <Col s={1} m={4}>
-                  <DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/>
-              </Col>
-            </Row>
-            <Row>
-              <Col s={1} m={4}>
-                  <DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/>
-              </Col>
-              <Col s={1} m={4}>
-                  <DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/>
-              </Col>
-              <Col s={1} m={4}>
-                  <DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/>
-              </Col>
-            </Row>
-        </Container>
+            {this.createBoard(this.props.cardData)}
+          </Container>
         </div>
-        
         );
+    }
+
+    createBoard = (data) => {
+      var i = 0;
+      var board = [];
+      var columns = data.map( cardData => <Col s={s} m={m}><DemoCard image={cardData.image} title={cardData.title} description={cardData.description} linkTo={cardData.linkTo} linkText={cardData.linkText}/></Col>);
+      var _columns = [];
+      var row = 1;
+      
+      columns.forEach(col => {
+        if(i < colNumber){
+          _columns.push(col);
+          i++;
+        }else{
+          i = 0;
+          board.push(<Row key={row++}>{_columns.slice(0)}</Row>);
+          _columns = [];
+          _columns.push(col);
+        }
+      });
+      if(_columns.length <= colNumber){
+        board.push(<Row key={row++}>{_columns.slice(0)}</Row>);
+      }
+      return board;
     }
 }
 
