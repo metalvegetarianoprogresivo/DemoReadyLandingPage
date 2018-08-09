14f90695cb6068a3195eb29ff5e26014583d67c3
Cristina Torres <30442276+cristito08@users.noreply.github.com>
2018-08-09 18:03:41 -0500

    Application of styles in demoPage components (#21)

--- src/components/demoPage/DemoPage.js
+++ src/components/demoPage/DemoPage.js
@@ -3,20 +3,23 @@ import HeaderDemo from '../../components/headerDemo/HeaderDemo'
 import VideoDemo from '../../components/VideoDemo/VideoDemo'
 import StudyCase from '../../components/StudyCase/StudyCase'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import SideNav from '../sidenav/sidenav.js'
 import './DemoPage.css';
 
 class DemoPage extends Component {
     render() {
         // TO-DO src should be rendered from props
         return [
-            <div class="container">
+            <div>
                 <HeaderDemo></HeaderDemo>
-                <Switch>
-                    <Route exact path="/" component={VideoDemo} />
-                    <Route exact path="/case" component={StudyCase} />
-                    <Route exact path = "/demo" component = {VideoDemo} />
-                </Switch>
+                {/* <SideNav></SideNav> */}
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={VideoDemo} />
+                        <Route exact path="/case" component={StudyCase} />
+                        <Route exact path = "/demo" component = {VideoDemo} />
+                    </Switch>
+                </div>
             </div>
         ]
     }
