e883fafe87e71d4e233c11d3b12d2f42e8a65776
Estefania Ocampo <eocampo@intersysconsulting.com>
2018-08-13 09:31:27 -0500

    fix url bug

--- src/Router.js
+++ src/Router.js
@@ -3,23 +3,30 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import DemoPage from './components/demoPage/DemoPage';
 import VideoDemo from './components/VideoDemo/VideoDemo';
+import HeaderDemo from './components/headerDemo/HeaderDemo';
 import StudyCase from './components/StudyCase/StudyCase';
 import Credits from './components/credits/Credits';
 import App from './App';
 import TechStack from './components/techStack/TechStack';
 
 class Router extends Component{
+  constructor(props) {
+    super(props)
+  }
+
     render() {
+      console.log(this)
         return(
           <BrowserRouter>
             <Switch>
               <Route exact path='/' component={App} />
-              <Route exact path='/demo-page/:category/:demoId' component={DemoPage} />
-              <Route exact path='/demo-page/:category/:demoId/demo' component={VideoDemo} />
+              <Route exact path='/demo-page/:category/:demoId' render={props => (<div><DemoPage/></div>)}/>}/>
+              <Route exact path='/demo-page/:category/:demoId/demo' render={props => (<div><HeaderDemo/><VideoDemo/></div>)}/>} />
               <Route exact path='/demo-page/:category/:demoId/case' component={StudyCase} />
               <Route exact path='/demo-page/:category/:demoId/credits' component={StudyCase} />
               <Route exact path='/demo-page/:category/:demoId/tech' component={TechStack} />
-              {/* <Route exact path='/demo-page/:pokemon' component={VideoDemo} /> */}
+            
+              <Route exact path='/demo-page/:pokemon' render={(props) => <VideoDemo {...props} isAuthed={true} />} />
             </Switch>
           </BrowserRouter>
         );
--- src/components/VideoDemo/VideoDemo.js
+++ src/components/VideoDemo/VideoDemo.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import HeaderDemo from '../../components/headerDemo/HeaderDemo'
 import './VideoDemo.css';
 import DemoPage from '../demoPage/DemoPage';
-
+//import React, {Component} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 class VideoDemo extends Component {
     constructor(props) {
         super(props)
+    }
+
+    componentDidMount() {
+        //const { match: { params } } = this.props;
+        console.log(this.props)
     }
 
     render() {
         // TO-DO src should be rendered from props
         return [
             <div class="video-container">
-                <DemoPage />
                 <iframe src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
             </div>
         ]
