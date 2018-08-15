f626b03bc33dc3a77fddcb9a2998ac0e718172c6
Estefanía Ocampo <deov31@gmail.com>
2018-08-15 10:52:36 -0500

    add sass (#33)

--- src/components/headerDemo/HeaderDemo.js
+++ src/components/headerDemo/HeaderDemo.js
@@ -1,34 +1,54 @@
-import React from 'react';
-import { Navbar, NavItem, Icon } from 'react-materialize'
+import React, { Component } from 'react';
+import { NavItem, Icon, Link, SideNavItem} from 'react-materialize'
 import './HeaderDemo.css';
 import logo from '../../assets/images/intersys-image.png';
-import { getUrl } from '../../../src/utils.js'
+import { getDataIds , 
+    matchParamUrl, 
+    getCategories , 
+    getDemosByCategory, 
+    getDemoById,
+    getUrl,
+    getLocation } from '../../../src/utils.js'
+import Sidenav from '../../components/sidenav/sidenav'
+import Navbar from '../../components/navbar/Navbar'
 
+let locationKeywords = getLocation();
 let currentPath = getUrl()
 
-let cleanUrl = function () {
-    // let currentPath = getUrl()
-    let arrPath = (currentPath.pathname.split('/'))
-    let path = arrPath.filter(word => word.length > 0)
-
-    if (path.length > 3) {
-        path.pop()
+class HeaderDemo extends Component {
+    constructor(props) {
+        super(props)
     }
 
-    return path.join('/')
+
+    cleanUrl = () => {
+        let currentPath = getUrl()
+        let arrPath = (currentPath.pathname.split('/'))
+        let path = arrPath.filter(word => word.length > 0)
+        
+        if (path.length > 3) {
+            path.pop()
+        }
+
+        return path.join('/')
+    }    
+
+    //
+
+    render() {
+        return [
+            <div class="header">
+                <Navbar brand={ <img href="/" className="navbar_logo" src={logo} /> } className='navbar_intersys' right >
+                    <NavItem href='/'><Icon>home</Icon></NavItem>
+                    <NavItem href={'/' + this.cleanUrl() + '/case'}>Case</NavItem>
+                    <NavItem href={'/' + this.cleanUrl() + '/demo'} src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0">Demo</NavItem>
+                    <NavItem href={'/' + this.cleanUrl() + '/credits'}>Credits</NavItem>
+                    <NavItem href={'/' + this.cleanUrl() + '/tech'}>Tech Stack</NavItem>
+                </Navbar>  
+                 
+            </div>
+        ]
+    }
 }
 
-const headerDemo = (props) => (
-    <div>
-        <Navbar brand={<img href="/" className="navbar_logo" src={logo} alt='logo' />} className='navbar_intersys' right >
-            <NavItem className="demos_menu"><Icon>menu</Icon></NavItem>
-            <NavItem href='/'><Icon>home</Icon></NavItem>
-            <NavItem href={`${currentPath.pathname}`}></NavItem>
-            <NavItem href={'/' + cleanUrl() + '/case'}>Case</NavItem>
-            <NavItem href={'/' + cleanUrl() + '/demo'} >Demo</NavItem>
-            <NavItem href={'/' + cleanUrl() + '/credits'}>Credits</NavItem>
-            <NavItem href={'/' + cleanUrl() + '/tech'}>Tech Stack</NavItem>
-        </Navbar>
-    </div>
-);
-export default headerDemo;
+export default HeaderDemo;
