2dae47734636bf2d02a73d20b80bb0fc9fe8c78f
david-intersys <40832991+david-intersys@users.noreply.github.com>
2018-08-16 18:04:52 -0500

    Partnership reference fix (#36)
    * Routes for demo page updated
    
    * fix url bug
    
    * updating branch based on comments
    
    * Routes and DemoPage's code refactor.
    
    * Code updated
    
    * Styles and text format added
    
    * Code refactor and warnings fixed
    
    * Card styles update
    
    * Cardboard moved to containers folder
    
    * Card size for mobile fixed
    
    * Hoverable cards
    
    * CSS import fixed

--- src/components/card/DemoCard.js
+++ src/components/card/DemoCard.js
@@ -7,7 +7,7 @@ const demoCard = (props) => (
     header={<CardTitle image={props.image} ></CardTitle>}
     actions={[<a key={props.linkText} href={props.linkTo}>{props.linkText}</a>]}
     title={props.title}
-    className='demo-card'
+    className='demo-card hoverable'
     reveal={<p dangerouslySetInnerHTML={{ __html: props.description.replace(/\n/g, '<br/>') }}></p>}>
   </Card>
 );
