f1da52f9441f9e65cc3ea6327a40adda1facca62
Nestor Lopez <nlopez@intersysconsulting.com>
2018-08-16 17:30:52 -0500

    modfiy call to action section

--- src/components/calltoaction/CallToAction.js
+++ src/components/calltoaction/CallToAction.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Parallax} from 'react-materialize';
+import {Button, Parallax, Row} from 'react-materialize';
 import './CallToAction.css'
 
 import landing from "../../assets/images/landing.jpg";
@@ -7,37 +7,33 @@ import landing from "../../assets/images/landing.jpg";
 class CallToAction extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Parallax className="banner__parallax" imageSrc={landing}/>
-        <div className="banner">
-          <div className="banner__video">
+      <div className="banner row">
+        <div className="col s12 m6">
+          <div className="video-container">
             <iframe
               title='Lets Do Business'
-              width="540"
-              height="295"
-              className="pitch"
+              height='295'
+              width='540'
               src="https://www.youtube.com/embed/SNoK5pyK73c?rel=0"
-              frameBorder="0"
-              allow="encrypted-media"
               allowFullScreen></iframe>
           </div>
-          <div className="banner__text-container">
-            <h2>Let's do
-              <b> Business</b>
-            </h2>
-            <div className="banner__underline"></div>
-            <div className="banner__paragraph">Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Aliquam cursus, est ac aliquam faucibus, quam lacus viverra mi,
-              sed porta tellus est id nisi. Cras velit diam, fermentum a nibh vel, dapibus
-              pellentesque justo. Nunc malesuada risus eget purus interdum volutpat.</div>
-            <div>
-              <Button node='a' href='#' className='banner__btn'>
-                Go</Button>
-            </div>
+        </div>
+        <div className="banner__text-container col s12 m6">
+          <h2>Let's do
+            <b>
+              Business</b>
+          </h2>
+          <div className="banner__underline"></div>
+          <div className="banner__paragraph">Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Aliquam cursus, est ac aliquam faucibus, quam lacus viverra mi,
+            sed porta tellus est id nisi. Cras velit diam, fermentum a nibh vel, dapibus
+            pellentesque justo. Nunc malesuada risus eget purus interdum volutpat.</div>
+          <div>
+            <Button node='a' href='#' className='banner__btn'>
+              Go</Button>
           </div>
         </div>
-      </React.Fragment>
-
+      </div>
     );
   }
 }
