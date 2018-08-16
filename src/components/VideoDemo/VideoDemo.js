a39a8f3db35d56ecba78e2cdf4a31bc4a3337dab
Nestor Lopez <nlopez@intersysconsulting.com>
2018-08-16 17:30:19 -0500

    modify css specificity

--- src/components/VideoDemo/VideoDemo.js
+++ src/components/VideoDemo/VideoDemo.js
@@ -1,10 +1,14 @@
-import React  from 'react';
+import React from 'react';
 import './VideoDemo.css';
 
-const videoDemo = (props) =>(
-            <div class="video-container">
-                <iframe src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" title='Video Demo' frameborder="0" allowfullscreen></iframe>
-            </div>
+const videoDemo = (props) => (
+  <div class="demo video-container">
+    <iframe
+      src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0"
+      title='Video Demo'
+      frameborder="0"
+      allowfullscreen></iframe>
+  </div>
 );
 
 export default videoDemo;
