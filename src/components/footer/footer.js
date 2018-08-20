249435bbd5aba73d6c320f50aa9fdf12d28b0e65
Ana Cristina Torres Vargas <cristi_torres94@hotmail.com>
2018-08-20 10:48:44 -0500

    change footer style

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,18 +9,18 @@ class Credits extends Component {
             <Footer copyrights='© 2018 Intersys Consulting, Inc. All Rights Reserved.'
             links={
                 <ul className='social-media'>
-                    <li><a href='#'><FaFacebookSquare /></a></li>
-                    <li><a href='#'><FaTwitterSquare /></a></li>
-                    <li><a href='#'><FaLinkedin /></a></li>
+                    <li><a href='https://www.facebook.com/IntersysConsulting'><FaFacebookSquare /></a></li>
+                    <li><a href='https://twitter.com/IntersysConsult'><FaTwitterSquare /></a></li>
+                    <li><a href='https://www.linkedin.com/company/intersys-consulting-inc-'><FaLinkedin /></a></li>
 
                 </ul>
                 
             }
             >
             <ul className='footer-links'>
-                <li><a className="grey-text text-lighten-3" href="#!">Company</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Partners</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Privacy Policy & Terms of Use</a></li>
+                <li><a className="grey-text text-lighten-3" href="https://www.intersysconsulting.com/company">Company</a></li>
+                <li><a className="grey-text text-lighten-3" href="https://www.intersysconsulting.com/company/partners">Partners</a></li>
+                <li><a className="grey-text text-lighten-3" href="https://www.intersysconsulting.com/company/privacy-policy-terms-use">Privacy Policy & Terms of Use</a></li>
             </ul>
             </Footer>
         );
