e01b6c539ca8bc948c43905921376694531b9044
Ana Cristina Torres Vargas <cristi_torres94@hotmail.com>
2018-08-21 16:51:25 -0500

    make responsive footer

--- src/components/consultantCard/consultantCard.js
+++ src/components/consultantCard/consultantCard.js
@@ -8,7 +8,7 @@ class ConsultantCard extends Component {
     render() {
         return [
             <Col s={12} m={6} l={5}>
-                <Card horizontal header={<CardTitle image={profile}></CardTitle>} actions={[<a href='#'>Go to Bio</a>]}>
+                <Card horizontal header={<CardTitle image={profile}></CardTitle>} className='hoverable' actions={[<a href='#'>Go to Bio</a>]}>
                     <h1>Consultant Name</h1>
                     <h2>Contribution to the project</h2>
                 </Card>
